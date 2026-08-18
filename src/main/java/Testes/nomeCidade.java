9fdb50ea8f1b1bc230e2cb3fb89f3b8b569fa8d5
Francisco Antonio <franciscoantonio53189@gmai.com>
2026-08-18 11:40:31 -0300

    test

---
+++ src/main/java/Testes/nomeCidade.java
@@ -0,0 +1,12 @@
+package Testes;
+
+public class nomeCidade {
+    public static void main (String[] args){
+        String nome = "francisco";
+        String cdd = "Blumenau";
+                String estado = "Santa Catarina";
+
+        System.out.println(" Meu nome é " + nome + ", sou de " + cdd + " e meu estado que eu moro é " + estado);
+
+    }
+}
