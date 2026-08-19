57d6d85e551796349e7bc1f3368d5c1439ecc7d5
Francisco Antonio <franciscoantonio53189@gmai.com>
2026-08-19 09:13:11 -0300

    atualizando ptojetos

---
+++ src/main/java/Testes/doisNumeros.java
@@ -0,0 +1,10 @@
+package Testes;
+
+public class doisNumeros {
+    public static void main (String[] args){
+        System.out.println(2 + 3 * 4);
+        System.out.println((2 + 3)* 4 );
+
+
+    }
+}
